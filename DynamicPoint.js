9bd455cf896fd597aae8d9ff8c8639e164c03054
LI Yu <liyu@clustertech.com>
2014-11-07 17:17:50 +0800

    revealjs now works

--- DynamicPoint.js
+++ DynamicPoint.js
@@ -1,18 +1,7 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
+  Meteor.startup(function () {
+    // code to run on client at startup
+    Reveal.initialize();
   });
 }
 
